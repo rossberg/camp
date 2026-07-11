8a3569df0b3eb6572d1f6a8bec02b3649b1e80ab
Andreas Rossberg <rossberg@mpi-sws.org>
2026-07-11 13:34:30 +0200

    2D dividers at borders

--- src/layout.ml
+++ src/layout.ml
@@ -350,6 +350,8 @@ let cover_popup_text g (p, x, y, w, _) ih = Ui.ticker g.ui (p, x, y + ih + pad_h
 let pdp = cp + 1
 let extension_divider_h_pane g = Ui.pane g.ui pdp (playlist_x g, playlist_y g, playlist_w g, divider_w g)
 let extension_divider_h g = Ui.divider g.ui (pdp, margin g, 0, - margin g, -1) `Vertical
+let extension_divider_wh_left g = Ui.divider2 g.ui (pdp, 0, 0, margin g, -1) `NE_SW
+let extension_divider_wh_right g = Ui.divider2 g.ui (pdp, - margin g, 0, -1, -1) `NW_SE
 
 let ldp = pdp + 1
 let extension_divider_x g = (if extension_left g then control_x else library_x) g
@@ -357,6 +359,8 @@ let extension_divider_w_pane g = Ui.pane g.ui ldp (extension_divider_x g, librar
 let extension_divider_w_upper g = Ui.divider g.ui (ldp, 0, margin g, -1, playlist_y g - library_y g - margin g) `Horizontal
 let extension_divider_wh g = Ui.divider2 g.ui (ldp, 0, playlist_y g - library_y g, -1, divider_w g) (if extension_left g then `NE_SW else `NW_SE)
 let extension_divider_w_lower g = Ui.divider g.ui (ldp, 0, playlist_y g - library_y g + divider_w g, -1, - margin g) `Horizontal
+let extension_divider_wh_upper g = Ui.divider2 g.ui (ldp, 0, 0, -1, margin g) (if extension_left g then `NW_SE else `NE_SW)
+let extension_divider_wh_lower g = Ui.divider2 g.ui (ldp, 0, - margin g, -1, -1) (if extension_left g then `NE_SW else `NW_SE)
 
 
 (* Playlist Pane *)
